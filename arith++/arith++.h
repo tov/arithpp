2a6ceefca12edaf06977eb85e99156de778ad36a
Jesse A. Tov <jesse.tov@gmail.com>
2018-01-29 16:49:38 -0600

    Basics seem to work.

--- arith++/arith++.h
+++ arith++/arith++.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include <limits>
 #include <stdexcept>
 
@@ -8,129 +9,377 @@ namespace arithpp {
 template<class T>
 struct Make_unsigned;
 
-#define DECLARE_MAKE_UNSIGNED(S, U) \
+#define SPECIALIZE_MAKE_UNSIGNED(S, U) \
     template <> struct Make_unsigned<S> { using type = U; }
 
-DECLARE_MAKE_UNSIGNED(char, unsigned char);
-DECLARE_MAKE_UNSIGNED(signed char, unsigned char);
-DECLARE_MAKE_UNSIGNED(short, unsigned short);
-DECLARE_MAKE_UNSIGNED(int, unsigned int);
-DECLARE_MAKE_UNSIGNED(long, unsigned long);
-DECLARE_MAKE_UNSIGNED(long long, unsigned long long);
+SPECIALIZE_MAKE_UNSIGNED(char, unsigned char);
+SPECIALIZE_MAKE_UNSIGNED(signed char, unsigned char);
+SPECIALIZE_MAKE_UNSIGNED(short, unsigned short);
+SPECIALIZE_MAKE_UNSIGNED(int, unsigned int);
+SPECIALIZE_MAKE_UNSIGNED(long, unsigned long);
+SPECIALIZE_MAKE_UNSIGNED(long long, unsigned long long);
 
 template<class T>
 using make_unsigned_t = typename Make_unsigned<T>::type;
 
-template<class T>
-inline T safe_negate(T a)
+template <class T>
+struct Saturating_policy
 {
-    if (a == std::numeric_limits<T>::min())
-        throw std::overflow_error("safe_negate overflowed positive");
-
-    return -a;
-}
-
-template<class T>
-inline make_unsigned_t<T> safe_abs(T a)
-{
-    if (a == std::numeric_limits<T>::min()) {
-        return make_unsigned_t<T>(std::numeric_limits<T>::max()) + 1;
-    } else if (a < 0) {
-        return -a;
-    } else {
-        return a;
-    }
-}
-
-template<class T>
-inline T safe_plus(T a, T b)
-{
-    // Bad cases are:
-    //  *  a + b > T_MAX  (can only happen if both are positive)
-    //  -  a + b < T_MIN  (can only happen if both are negative)
-    if (b >= 0) {
-        // -  a + b > T_MAX  ===  a > T_MAX - b
-        // The latter cannot overflow of b is non-negative.
-        if (a > std::numeric_limits<T>::max() - b) {
-            throw std::overflow_error("safe_plus overflowed positive");
-        }
-        // -  a + b < T_MIN  ===  a < T_MIN - b
-        // The latter cannot overflow if b is negative.
-    } else {
-        if (a < std::numeric_limits<T>::min() - b) {
-            throw std::overflow_error("safe_plus overflowed negative");
-        }
+    static T too_big(const char*)
+    {
+        return std::numeric_limits<T>::max();
     }
 
-    return a + b;
-}
+    static T too_small(const char*)
+    {
+        return std::numeric_limits<T>::min();
+    }
+};
 
-template<class T>
-inline T safe_minus(T a, T b)
+template <class T, class E = std::overflow_error>
+struct Throwing_policy
 {
-    // Bad cases are:
-    //  -  a - b > T_MAX  (can only happen if a > 0 and b < 0)
-    //  -  a - b < T_MIN  (can only happen if a < 0 and b > 0)
-    if (a >= 0) {
-        //  -  a - b > T_MAX  ===  b < a - T_MAX
-        // The latter cannot overflow if a is non-negative.
-        if (b < a - std::numeric_limits<T>::max()) {
-            throw std::overflow_error("safe_plus overflowed positive");
-        }
-    } else {
-        //  -  a - b < T_MIN  ===  b > a - T_MIN
-        if (b > a - std::numeric_limits<T>::min()) {
-            throw std::overflow_error("safe_plus overflowed negative");
-        }
+    static T too_big(const char* who)
+    {
+        throw E(who);
     }
 
-    return a - b;
-}
+    static T too_small(const char* who)
+    {
+        throw E(who);
+    }
+};
 
-template<class T>
-inline T safe_times(T a, T b)
+template <class T, class P = Throwing_policy<T>>
+class Checked
 {
-    // Bad cases are:
-    //  -  a * b > T_MAX  (can only happen if signs are same)
-    //  -  a * b < T_MIN  (can only happen if signs are different)
-    if (b > 0) {
-        //  - a * b > T_MAX  ===   a > T_MAX / b
-        // Can do this safely if b is positive.
-        if (a > std::numeric_limits<T>::max() / b) {
-            throw std::overflow_error("safe_times overflowed positive");
-        }
+private:
+    using unsigned_t = make_unsigned_t<T>;
+    T value_;
 
-        //  - a * b < T_MIN  ===   a < T_MIN / b
-        // Can do this safely if b is positive.
-        if (a < std::numeric_limits<T>::min() / b) {
-            throw std::overflow_error("safe_times overflowed negative");
-        }
-    } else if (a > 0) {
-        // By symmetry:
-        if (b > std::numeric_limits<T>::max() / a) {
-            throw std::overflow_error("safe_times overflowed positive");
-        }
+    static T min_()
+    {
+        return std::numeric_limits<T>::min();
+    }
 
-        if (b < std::numeric_limits<T>::min() / a) {
-            throw std::overflow_error("safe_times overflowed negative");
-        }
-    } else if (a < 0 && b < 0) {
-        if (safe_abs(a) > std::numeric_limits<T>::max() / safe_abs(b)) {
-            throw std::overflow_error("safe_times overflowed positive");
+    static T max_()
+    {
+        return std::numeric_limits<T>::max();
+    }
+
+public:
+    Checked(T value) : value_(value)
+    { }
+
+    T get() const { return value_ ; }
+
+    Checked operator-() const
+    {
+        if (value_ == min_())
+            return P::too_big("Checked::operator-()");
+        else
+            return -value_;
+    }
+
+    unsigned_t abs() const
+    {
+        if (value_ == min_()) {
+            return unsigned_t(std::numeric_limits<T>::max()) + 1;
+        } else if (value_ < 0) {
+            return unsigned_t(-value_);
+        } else {
+            return unsigned_t(value_);
         }
     }
 
-    return a * b;
-}
+    Checked operator+(Checked other) const
+    {
+        if (other.value_ >= 0) {
+            if (value_ > max_() - other.value_)
+                return P::too_big("Checked::operator+");
+        } else {
+            if (value_ < min_() - other.value_)
+                return P::too_small("Checked::operator+");
+        }
 
-template<class T>
-T safe_divides(T a, T b)
-{
-    if (a == std::numeric_limits<T>::min() && b == -1) {
-        throw std::overflow_error("safe_divides overflowed positive");
+        return value_ + other.value_;
     }
 
-    return a / b;
+    Checked operator-(Checked other) const
+    {
+        if (value_ >= 0) {
+            if (other.value_ < value_ - max_())
+                return P::too_big("Checked::operator-");
+        } else {
+            if (other.value_ > value_ - min_())
+                return P::too_small("Checked::operator-");
+        }
+
+        return value_ - other.value_;
+    }
+
+    // This is slow right now, because it does a division. There are better
+    // ways of doing it, depending on the size of T.
+    Checked operator*(Checked other) const
+    {
+        if (other.value_ != 0) {
+            if (abs() > unsigned_t(max_()) / other.abs()) {
+                if ((value_ > 0 && other.value_ > 0) ||
+                        (value_ < 0 && other.value_ < 0))
+                    return P::too_big("Checked::operator*");
+                else
+                    return P::too_small("Checked::operator*");
+            }
+        }
+
+        return value_ * other.value_;
+    }
+
+    Checked operator/(Checked other) const
+    {
+        if (value_ == min_() && other.value_ == -1)
+            return P::too_big("Checked::operator/");
+
+        return value_ / other.value_;
+    }
+
+    // Should this do some kind of checking?
+    Checked operator%(Checked other) const
+    {
+        return value_ % other.value_;
+    }
+
+};
+
+template <class T, class P>
+bool operator==(Checked<T, P> a, Checked<T, P> b)
+{
+    return a.get() == b.get();
+}
+
+template <class T, class P>
+bool operator!=(Checked<T, P> a, Checked<T, P> b)
+{
+    return a.get() != b.get();
+}
+
+template <class T, class P>
+bool operator<(Checked<T, P> a, Checked<T, P> b)
+{
+    return a.get() < b.get();
+}
+
+template <class T, class P>
+bool operator>(Checked<T, P> a, Checked<T, P> b)
+{
+    return a.get() > b.get();
+}
+
+template <class T, class P>
+bool operator<=(Checked<T, P> a, Checked<T, P> b)
+{
+    return a.get() <= b.get();
+}
+
+template <class T, class P>
+bool operator>=(Checked<T, P> a, Checked<T, P> b)
+{
+    return a.get() >= b.get();
+}
+
+template <class T, class P>
+std::ostream& operator<<(std::ostream& o, Checked<T, P> a)
+{
+    return o << a.get();
+}
+
+template <class T, class P>
+std::istream& operator>>(std::istream& i, Checked<T, P>& a) {
+    T temp;
+    i >> temp;
+    a = Checked<T, P>(temp);
+    return i;
+}
+
+template <class T>
+using Saturating = Checked<T, Saturating_policy<T>>;
+
+template <class T>
+class Wrapping
+{
+private:
+    using unsigned_t = make_unsigned_t<T>;
+    unsigned_t value_;
+
+    explicit Wrapping(unsigned_t value) : value_(value)
+    { }
+
+public:
+    Wrapping(T value) : value_(static_cast<unsigned_t>(value))
+    { }
+
+    T get() const { return static_cast<T>(value_); }
+
+    Wrapping operator-() const {
+        if (get() == std::numeric_limits<T>::min())
+            return Wrapping(std::numeric_limits<T>::min());
+        else
+            return Wrapping(-get());
+    }
+
+    unsigned_t abs() const
+    {
+        if (get() == std::numeric_limits<T>::min()) {
+            return unsigned_t(std::numeric_limits<T>::max()) + 1;
+        } else if (get() < 0) {
+            return static_cast<unsigned_t>(-get());
+        } else {
+            return static_cast<unsigned_t>(get());
+        }
+    }
+
+    Wrapping operator+(Wrapping other) const {
+        return Wrapping(value_ + other.value_);
+    }
+
+    Wrapping operator-(Wrapping other) const {
+        return Wrapping(value_ - other.value_);
+    }
+
+    Wrapping operator*(Wrapping other) const {
+        return Wrapping(value_ * other.value_);
+    }
+
+    Wrapping operator/(Wrapping other) const {
+        return Wrapping(value_ / other.value_);
+    }
+
+    Wrapping operator%(Wrapping other) const {
+        return Wrapping(value_ % other.value_);
+    }
+
+    Wrapping operator&(Wrapping other) const {
+        return Wrapping(value_ & other.value_);
+    }
+
+    Wrapping operator|(Wrapping other) const {
+        return Wrapping(value_ | other.value_);
+    }
+
+    Wrapping operator^(Wrapping other) const {
+        return Wrapping(value_ ^ other.value_);
+    }
+
+    Wrapping operator<<(u_int8_t other) const {
+        return Wrapping(value_ << other);
+    }
+
+    Wrapping operator>>(u_int8_t other) const {
+        return Wrapping(get() >> other);
+    }
+
+    Wrapping operator~() const {
+        return Wrapping(~value_);
+    }
+
+    Wrapping& operator+=(Wrapping other) {
+        value_ += other.value_;
+        return *this;
+    }
+
+    Wrapping& operator-=(Wrapping other) {
+        value_ -= other.value_;
+        return *this;
+    }
+
+    Wrapping& operator*=(Wrapping other) {
+        value_ *= other.value_;
+        return *this;
+    }
+
+    Wrapping& operator/=(Wrapping other) {
+        value_ /= other.value_;
+        return *this;
+    }
+
+    Wrapping& operator%=(Wrapping other) {
+        value_ %= other.value_;
+        return *this;
+    }
+
+    Wrapping& operator&=(Wrapping other) {
+        value_ &= other.value_;
+        return *this;
+    }
+
+    Wrapping& operator|=(Wrapping other) {
+        value_ |= other.value_;
+        return *this;
+    }
+
+    Wrapping& operator^=(Wrapping other) {
+        value_ ^= other.value_;
+        return *this;
+    }
+
+    Wrapping& operator<<=(Wrapping other) {
+        value_ <<= other.value_;
+        return *this;
+    }
+
+    Wrapping& operator>>=(Wrapping other) {
+        return *this = Wrapping(get() >> other);
+    }
+};
+
+template <class T>
+bool operator==(Wrapping<T> a, Wrapping<T> b)
+{
+    return a.get() == b.get();
+}
+
+template <class T>
+bool operator!=(Wrapping<T> a, Wrapping<T> b)
+{
+    return a.get() != b.get();
+}
+
+template <class T>
+bool operator<(Wrapping<T> a, Wrapping<T> b)
+{
+    return a.get() < b.get();
+}
+
+template <class T>
+bool operator>(Wrapping<T> a, Wrapping<T> b)
+{
+    return a.get() > b.get();
+}
+
+template <class T>
+bool operator<=(Wrapping<T> a, Wrapping<T> b)
+{
+    return a.get() <= b.get();
+}
+
+template <class T>
+bool operator>=(Wrapping<T> a, Wrapping<T> b)
+{
+    return a.get() >= b.get();
+}
+
+template <class T>
+std::ostream& operator<<(std::ostream& o, Wrapping<T> a)
+{
+    return o << a.get();
+}
+
+template <class T>
+std::istream& operator>>(std::istream& i, Wrapping<T>& a) {
+    T temp;
+    i >> temp;
+    a = Wrapping<T>(temp);
+    return i;
 }
 
 }
--- test/arithmetic_test.cpp
+++ test/arithmetic_test.cpp
@@ -1,77 +1,109 @@
 #include "arith++/arith++.h"
 #include "UnitTest++/UnitTest++.h"
 
-using namespace arithpp;
+using W = arithpp::Wrapping<int>;
+using C = arithpp::Checked<int>;
+using S = arithpp::Saturating<int>;
 
 int main()
 {
     return UnitTest::RunAllTests();
 }
 
-TEST(Safe_negate) {
-    CHECK_EQUAL(8, safe_negate(-8));
-    CHECK_EQUAL(-7, safe_negate(7));
-    CHECK_EQUAL(-INT_MAX, safe_negate(INT_MAX));
-    CHECK_EQUAL(INT_MAX, safe_negate(-INT_MAX));
-    CHECK_THROW(safe_negate(INT_MIN), std::overflow_error);
+TEST(Wrapping) {
+    CHECK_EQUAL(5, W(5).get());
+    CHECK_EQUAL(W(INT_MIN + 4), W(INT_MAX) + W(5));
 }
 
-TEST(Safe_abs) {
-    CHECK_EQUAL(3, safe_abs(3));
-    CHECK_EQUAL(3, safe_abs(-3));
-    CHECK_EQUAL(INT_MAX, safe_abs(INT_MAX));
-    CHECK_EQUAL(INT_MAX, safe_abs(-INT_MAX));
-    CHECK_EQUAL(unsigned(INT_MAX) + 1, safe_abs(INT_MIN));
+TEST(Checked_construction) {
+    CHECK_EQUAL(5, C(5).get());
 }
 
-TEST(Safe_plus) {
-    CHECK_EQUAL(8, safe_plus(3, 5));
-    CHECK_EQUAL(-1, safe_plus(INT_MIN, INT_MAX));
-    CHECK_THROW(safe_plus(INT_MAX, INT_MAX), std::overflow_error);
-    CHECK_THROW(safe_plus(INT_MIN, INT_MIN), std::overflow_error);
-    CHECK_THROW(safe_plus(INT_MAX, 1), std::overflow_error);
-    CHECK_THROW(safe_plus(1, INT_MAX), std::overflow_error);
-    CHECK_THROW(safe_plus(INT_MAX - 5, 6), std::overflow_error);
-    CHECK_THROW(safe_plus(INT_MIN, -1), std::overflow_error);
+TEST(Checked_negate) {
+    CHECK_EQUAL(C(-5), -C(5));
+    CHECK_EQUAL(C(-INT_MAX), -C(INT_MAX));
+    CHECK_EQUAL(C(INT_MAX), -C(-INT_MAX));
+    CHECK_THROW(-C(INT_MIN), std::overflow_error);
 }
 
-TEST(Safe_minus) {
-    CHECK_EQUAL(3, safe_minus(8, 5));
-    CHECK_EQUAL(-3, safe_minus(5, 8));
-    CHECK_EQUAL(-13, safe_minus(-5, 8));
-    CHECK_EQUAL(13, safe_minus(8, -5));
-    CHECK_EQUAL(-13, safe_minus(-8, 5));
-    CHECK_EQUAL(13, safe_minus(5, -8));
-    CHECK_EQUAL(3, safe_minus(-5, -8));
-    CHECK_EQUAL(-3, safe_minus(-8, -5));
-    CHECK_EQUAL(INT_MAX - 3, safe_minus(INT_MAX, 3));
-    CHECK_EQUAL(INT_MAX, safe_minus(INT_MAX - 2, -2));
-    CHECK_EQUAL(INT_MIN, safe_minus(INT_MIN + 3, 3));
-
-    CHECK_THROW(safe_minus(INT_MAX, -1), std::overflow_error);
-    CHECK_THROW(safe_minus(INT_MAX - 2, -3), std::overflow_error);
-    CHECK_THROW(safe_minus(INT_MIN + 2, 3), std::overflow_error);
+TEST(Saturating_negate) {
+    CHECK_EQUAL(S(-5), -S(5));
+    CHECK_EQUAL(S(-INT_MAX), -S(INT_MAX));
+    CHECK_EQUAL(S(INT_MAX), -S(INT_MIN));
 }
 
-TEST(Safe_times) {
-    CHECK_EQUAL(12, safe_times(3, 4));
-    CHECK_EQUAL(-12, safe_times(-3, 4));
-    CHECK_EQUAL(-12, safe_times(3, -4));
-    CHECK_EQUAL(12, safe_times(-3, -4));
-
-    CHECK_EQUAL(INT_MAX, safe_times(INT_MAX / 2, 2) + INT_MAX % 2);
-    CHECK_EQUAL(INT_MAX, safe_times(INT_MAX / 5, 5) + INT_MAX % 5);
-    CHECK_THROW(safe_times(INT_MAX / 5, 6), std::overflow_error);
-    CHECK_THROW(safe_times(INT_MAX / 5, -6), std::overflow_error);
-    CHECK_THROW(safe_times(INT_MAX / -5, 6), std::overflow_error);
-    CHECK_THROW(safe_times(INT_MAX / -5, -6), std::overflow_error);
+TEST(Checked_abs) {
+    CHECK_EQUAL(3, C(3).abs());
+    CHECK_EQUAL(3, C(-3).abs());
+    CHECK_EQUAL(INT_MAX, C(INT_MAX).abs());
+    CHECK_EQUAL(INT_MAX, C(-INT_MAX).abs());
+    CHECK_EQUAL(unsigned(INT_MAX) + 1, C(INT_MIN).abs());
 }
 
-TEST(Safe_divides) {
-    CHECK_EQUAL(3, safe_divides(12, 4));
-    CHECK_EQUAL(-3, safe_divides(12, -4));
-    CHECK_EQUAL(-3, safe_divides(-12, 4));
-    CHECK_EQUAL(3, safe_divides(-12, -4));
+TEST(Checked_plus) {
+    CHECK_EQUAL(C(8), C(3) + C(5));
+    CHECK_EQUAL(C(-1), C(INT_MIN) + C(INT_MAX));
+    CHECK_THROW(C(INT_MAX) + C(INT_MAX), std::overflow_error);
+    CHECK_THROW(C(INT_MIN) + C(INT_MIN), std::overflow_error);
+    CHECK_THROW(C(INT_MAX) + C(1), std::overflow_error);
+    CHECK_THROW(C(1) + C(INT_MAX), std::overflow_error);
+    CHECK_THROW(C(INT_MAX - 5) + C(6), std::overflow_error);
+    CHECK_THROW(C(INT_MIN) + C(-1), std::overflow_error);
+}
 
-    CHECK_THROW(safe_divides(INT_MIN, -1), std::overflow_error);
+TEST(Saturating_plus) {
+    CHECK_EQUAL(S(8), S(3) + S(5));
+    CHECK_EQUAL(S(-1), S(INT_MIN) + S(INT_MAX));
+    CHECK_EQUAL(S(INT_MAX), S(INT_MAX) + S(INT_MAX));
+    CHECK_EQUAL(S(INT_MIN), S(INT_MIN) + S(INT_MIN));
+    CHECK_EQUAL(S(INT_MAX), S(INT_MAX) + S(1));
+    CHECK_EQUAL(S(INT_MAX), S(1) + S(INT_MAX));
+    CHECK_EQUAL(S(INT_MAX), S(INT_MAX - 5) + S(6));
+    CHECK_EQUAL(S(INT_MIN), S(INT_MIN) + S(-1));
+}
+
+TEST(Checked_minus) {
+    CHECK_EQUAL(C(3), C(8) - C(5));
+    CHECK_EQUAL(C(-3), C(5) - C(8));
+    CHECK_EQUAL(C(-13), C(-5) - C(8));
+    CHECK_EQUAL(C(13), C(8) - C(-5));
+    CHECK_EQUAL(C(-13), C(-8) - C(5));
+    CHECK_EQUAL(C(13), C(5) - C(-8));
+    CHECK_EQUAL(C(3), C(-5) - C(-8));
+    CHECK_EQUAL(C(-3), C(-8) - C(-5));
+    CHECK_EQUAL(C(INT_MAX - 3), C(INT_MAX) - C(3));
+    CHECK_EQUAL(C(INT_MAX), C(INT_MAX - 2) - C(-2));
+    CHECK_EQUAL(C(INT_MIN), C(INT_MIN + 3) - C(3));
+
+    CHECK_THROW(C(INT_MAX) - C(-1), std::overflow_error);
+    CHECK_THROW(C(INT_MAX - 2) - C(-3), std::overflow_error);
+    CHECK_THROW(C(INT_MIN + 2) - C(3), std::overflow_error);
+}
+
+TEST(Checked_times) {
+    CHECK_EQUAL(C(12), C(3) * C(4));
+    CHECK_EQUAL(C(-12), C(-3) * C(4));
+    CHECK_EQUAL(C(-12), C(3) * C(-4));
+    CHECK_EQUAL(C(12), C(-3) * C(-4));
+
+    CHECK_EQUAL(C(INT_MAX), C(INT_MAX / 2) * 2 + INT_MAX % 2);
+    CHECK_EQUAL(C(INT_MAX), C(INT_MAX / 5) * 5 + INT_MAX % 5);
+    CHECK_THROW(C(INT_MAX / 5) * 6, std::overflow_error);
+    CHECK_THROW(C(INT_MAX / 5) * -6, std::overflow_error);
+    CHECK_THROW(C(INT_MAX / -5) * 6, std::overflow_error);
+    CHECK_THROW(C(INT_MAX / -5) * -6, std::overflow_error);
+}
+
+TEST(Saturating_times) {
+    CHECK_EQUAL(S(12), S(3) * S(4));
+    CHECK_EQUAL(S(-12), S(-3) * S(4));
+    CHECK_EQUAL(S(-12), S(3) * S(-4));
+    CHECK_EQUAL(S(12), S(-3) * S(-4));
+
+    CHECK_EQUAL(S(INT_MAX), S(INT_MAX / 2) * 2 + INT_MAX % 2);
+    CHECK_EQUAL(S(INT_MAX), S(INT_MAX / 5) * 5 + INT_MAX % 5);
+    CHECK_EQUAL(S(INT_MAX), S(INT_MAX / 5) * 6);
+    CHECK_EQUAL(S(INT_MIN), S(INT_MAX / 5) * -6);
+    CHECK_EQUAL(S(INT_MIN), S(INT_MAX / -5) * 6);
+    CHECK_EQUAL(S(INT_MAX), S(INT_MAX / -5) * -6);
 }
