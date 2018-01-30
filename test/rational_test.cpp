3e6ac5572fed9de17a767262ff123a25c348c7b3
Jesse A. Tov <jesse.tov@gmail.com>
2018-01-30 09:37:35 -0600

    Two rational tests.

--- test/rational_test.cpp
+++ test/rational_test.cpp
@@ -1,2 +1,16 @@
-#include <arith++/arith++.h>
+#include "Rational.h"
 #include <UnitTest++/UnitTest++.h>
+#include <stdexcept>
+
+using R = rational::Rational;
+
+TEST(RationalMultiplication)
+{
+    CHECK_EQUAL(R(1, 2), R(2, 3) * R(3, 4));
+    CHECK_EQUAL(R(LONG_MAX), R(LONG_MAX, 3) * R(3));
+}
+
+TEST(RationalMultiplicationOverflow)
+{
+    CHECK_THROW(R(LONG_MAX, 3) * R(5), std::overflow_error);
+}
